4838ab8a91a3b2dc5edf756bd87e4db233440249
akhilrb <akhil.r.baranwal@gmail.com>
2018-12-24 00:32:05 +0530

    added chatID option to track messages

--- master.py
+++ master.py
@@ -8,41 +8,41 @@ import getpass
 import paho.mqtt.publish as publish
 
 def getConfigFiles():
-	global configFiles
-	configFiles = []
-	files = os.listdir(os.path.join(os.getcwd(),'config'))
-	for file in files:
-		if file.endswith('.config'):
-			configFiles.append(file)
-			print(str(len(configFiles)) + ". " + configFiles[len(configFiles) - 1])
+    global configFiles
+    configFiles = []
+    files = os.listdir(os.path.join(os.getcwd(),'config'))
+    for file in files:
+        if file.endswith('.config'):
+            configFiles.append(file)
+            print(str(len(configFiles)) + ". " + configFiles[len(configFiles) - 1])
 
 def loadFromConfig():
-	global configFiles, serverChoice, mqttBrokerIP, mqttPort, mqttTopic, mqttUsername, mqttPassword, hostIP, hostPort, hostPassword
-	if(len(configFiles) > 0):
-		x = input("Do you want to load any previous configurations?<y/n> : ")
-		if(x == 'y'):
-			fileNumber = int(input("Enter corresponding number of file : "))
-			if((fileNumber) <= len(configFiles)):
-				shelfFile = shelve.open(os.path.join('config', configFiles[fileNumber - 1]))
-				if(list(shelfFile['data'])[0] == 1):
-					# public server
-					[serverChoice, mqttBrokerIP, mqttPort, mqttTopic, hostIP, hostPort, hostPassword] = list(shelfFile['data'])
-					return True
-				else:
-					# private server
-					[serverChoice, mqttBrokerIP, mqttPort, mqttTopic, mqttUsername, mqttPassword, hostIP, hostPort, hostPassword] = list(shelfFile['data'])
-					return True
-			else:
-				return False
-		else:
-			return False
+    global configFiles, serverChoice, mqttBrokerIP, mqttPort, mqttTopic, mqttUsername, mqttPassword, hostIP, hostPort, hostPassword
+    if(len(configFiles) > 0):
+        x = input("Do you want to load any previous configurations?<y/n> : ")
+        if(x == 'y'):
+            fileNumber = int(input("Enter corresponding number of file : "))
+            if((fileNumber) <= len(configFiles)):
+                shelfFile = shelve.open(os.path.join('config', configFiles[fileNumber - 1]))
+                if(list(shelfFile['data'])[0] == 1):
+                    # public server
+                    [serverChoice, mqttBrokerIP, mqttPort, mqttTopic, hostIP, hostPort, hostPassword] = list(shelfFile['data'])
+                    return True
+                else:
+                    # private server
+                    [serverChoice, mqttBrokerIP, mqttPort, mqttTopic, mqttUsername, mqttPassword, hostIP, hostPort, hostPassword] = list(shelfFile['data'])
+                    return True
+            else:
+                return False
+        else:
+            return False
 
 def printConfig():
-	print("MQTT Broker IP/Server: "+ mqttBrokerIP)
-	print("MQTT Port: "+ mqttPort)
-	print("MQTT Topic: "+ mqttTopic)
-	if(serverChoice == 2):
-		print("MQTT Username: "+ mqttUsername)
+    print("MQTT Broker IP/Server: "+ mqttBrokerIP)
+    print("MQTT Port: "+ mqttPort)
+    print("MQTT Topic: "+ mqttTopic)
+    if(serverChoice == 2):
+        print("MQTT Username: "+ mqttUsername)
 
 # if arguments have been passed, prepare list
 args = []
@@ -52,110 +52,112 @@ n = len(args)
 
 # command was directly executed
 if(n == 1):
-	getConfigFiles()
-	if(not loadFromConfig()):
-		print("1. Public Server \nor\n2. Private Server\nPublic Servers don't use a username and password.")
-		serverChoice = int(input("Choose (1 or 2): "))
-		if(serverChoice == 1):
-			mqttBrokerIP = input("MQTT Broker IP/Server: ")
-			mqttPort = input("MQTT Port: ")
-			mqttTopic = input("MQTT Topic: ")
-		elif(serverChoice == 2):
-			mqttBrokerIP = input("MQTT Broker IP: ")
-			mqttPort = input("MQTT Port: ")
-			mqttTopic = input("MQTT Topic: ")
-			mqttUsername = input("MQTT Username: ")
-			print("MQTT ", end='')
-			sys.stdout.flush()
-			mqttPassword = getpass.getpass()
-		else:
-			sys.exit()
-	else:
-		print("Loaded Configuration:")
-		printConfig()
+    getConfigFiles()
+    if(not loadFromConfig()):
+        print("1. Public Server \nor\n2. Private Server\nPublic Servers don't use a username and password.")
+        serverChoice = int(input("Choose (1 or 2): "))
+        if(serverChoice == 1):
+            mqttBrokerIP = input("MQTT Broker IP/Server: ")
+            mqttPort = input("MQTT Port: ")
+            mqttTopic = input("MQTT Topic: ")
+        elif(serverChoice == 2):
+            mqttBrokerIP = input("MQTT Broker IP: ")
+            mqttPort = input("MQTT Port: ")
+            mqttTopic = input("MQTT Topic: ")
+            mqttUsername = input("MQTT Username: ")
+            print("MQTT ", end='')
+            sys.stdout.flush()
+            mqttPassword = getpass.getpass()
+        else:
+            sys.exit()
+    else:
+        print("Loaded Configuration:")
+        printConfig()
 
 # command was executed with arguments
 elif(n == 6):
-	mqttBrokerIP = args[1]
-	mqttPort = args[2]
-	mqttTopic = args[3]
-	mqttUsername = args[4]
-	mqttPassword = args[5]
+    mqttBrokerIP = args[1]
+    mqttPort = args[2]
+    mqttTopic = args[3]
+    mqttUsername = args[4]
+    mqttPassword = args[5]
 
 # wrong number of arguments
 else:
-	print("Wrong number of arguments!")
-	print("Usage:\npython3 vlcMQTTSync.py\nor\npython3 vlcMQTTSync.py <brokerIP> <port> <topic> <username> <password>")
-	print("Exiting...")
-	sys.exit()
+    print("Wrong number of arguments!")
+    print("Usage:\npython3 vlcMQTTSync.py\nor\npython3 vlcMQTTSync.py <brokerIP> <port> <topic> <username> <password>")
+    print("Exiting...")
+    sys.exit()
+
+chatID = getpass.getuser()
 
 print("\033[0;38;2;0;192;0mReady\033[0m\nEnter 'h' for a list of available functions, or 'x' to exit")
 while True:
-	x = input("Message: ")
-	if(x == 'h'):
-		print("| add XYZ  . . . . . . . . . . . . . . . . . . . . add XYZ to playlist")
-		print("| enqueue XYZ  . . . . . . . . . . . . . . . . . queue XYZ to playlist")
-		print("| delete [X] . . . . . . . . . . . . . . . . delete item X in playlist")
-		print("| move [X][Y]  . . . . . . . . . . . . move item X in playlist after Y")
-		print("| sort key . . . . . . . . . . . . . . . . . . . . . sort the playlist")
-		print("| sd [sd]  . . . . . . . . . . . . . show services discovery or toggle")
-		print("| play . . . . . . . . . . . . . . . . . . . . . . . . . . play stream")
-		print("| stop . . . . . . . . . . . . . . . . . . . . . . . . . . stop stream")
-		print("| next . . . . . . . . . . . . . . . . . . . . . .  next playlist item")
-		print("| prev . . . . . . . . . . . . . . . . . . . .  previous playlist item")
-		print("| goto, gotoitem . . . . . . . . . . . . . . . . .  goto item at index")
-		print("| repeat [on|off]  . . . . . . . . . . . . . .  toggle playlist repeat")
-		print("| loop [on|off]  . . . . . . . . . . . . . . . .  toggle playlist loop")
-		print("| random [on|off]  . . . . . . . . . . . . . .  toggle playlist random")
-		print("| clear  . . . . . . . . . . . . . . . . . . . . .  clear the playlist")
-		print("| title [X]  . . . . . . . . . . . . . . set/get title in current item")
-		print("| chapter [X]  . . . . . . . . . . . . set/get chapter in current item")
-		print("| ")
-		print("| seek X . . . . . . . . . . . seek in seconds, for instance `seek 12'")
-		print("| pause  . . . . . . . . . . . . . . . . . . . . . . . .  toggle pause")
-		print("| fastforward  . . . . . . . . . . . . . . . . . . set to maximum rate")
-		print("| rewind . . . . . . . . . . . . . . . . . . . . . set to minimum rate")
-		print("| faster . . . . . . . . . . . . . . . . . .  faster playing of stream")
-		print("| slower . . . . . . . . . . . . . . . . . .  slower playing of stream")
-		print("| normal . . . . . . . . . . . . . . . . . .  normal playing of stream")
-		print("| rate [playback rate] . . . . . . . . . .  set playback rate to value")
-		print("| frame  . . . . . . . . . . . . . . . . . . . . . play frame by frame")
-		print("| fullscreen, f, F [on|off]  . . . . . . . . . . . . toggle fullscreen")
-		print("| ")
-		print("| volume [X] . . . . . . . . . . . . . . . . . .  set/get audio volume")
-		print("| volup [X]  . . . . . . . . . . . . . . .  raise audio volume X steps")
-		print("| voldown [X]  . . . . . . . . . . . . . .  lower audio volume X steps")
-		print("| achan [X]  . . . . . . . . . . . .  set/get stereo audio output mode")
-		print("| atrack [X] . . . . . . . . . . . . . . . . . . . set/get audio track")
-		print("| vtrack [X] . . . . . . . . . . . . . . . . . . . set/get video track")
-		print("| vratio [X] . . . . . . . . . . . . . . .  set/get video aspect ratio")
-		print("| vcrop, crop [X]  . . . . . . . . . . . . . . . .  set/get video crop")
-		print("| vzoom, zoom [X]  . . . . . . . . . . . . . . . .  set/get video zoom")
-		print("| vdeinterlace [X] . . . . . . . . . . . . . set/get video deinterlace")
-		print("| vdeinterlace_mode [X]  . . . . . . .  set/get video deinterlace mode")
-		print("| snapshot . . . . . . . . . . . . . . . . . . . . take video snapshot")
-		print("| strack [X] . . . . . . . . . . . . . . . . .  set/get subtitle track")
-		print("| ")
-		print("| vlm  . . . . . . . . . . . . . . . . . . . . . . . . .  load the VLM")
-		print("| logout . . . . . . . . . . . . . .  exit (if in a socket connection)")
-		print("| quit . . . . . . . .  quit VLC (or logout if in a socket connection)")
-		print("| shutdown . . . . . . . . . . . . . . . . . . . . . . .  shutdown VLC")
-		print()
-	elif(x == 'x'):
-		print("Exiting")
-		sys.exit()
-	else:
-		if(serverChoice == 1):
-			try:
-				publish.single(mqttTopic, x, hostname=mqttBrokerIP, port=int(mqttPort))
-			except Exception as e:
-				print("\033[1;38;2;227;32;32mError while sending message\033[0m")	
-				print(str(e)+"\n")
+    x = chatID + "/: " + input("Message: ")
+    if(x == 'h'):
+        print("| add XYZ  . . . . . . . . . . . . . . . . . . . . add XYZ to playlist")
+        print("| enqueue XYZ  . . . . . . . . . . . . . . . . . queue XYZ to playlist")
+        print("| delete [X] . . . . . . . . . . . . . . . . delete item X in playlist")
+        print("| move [X][Y]  . . . . . . . . . . . . move item X in playlist after Y")
+        print("| sort key . . . . . . . . . . . . . . . . . . . . . sort the playlist")
+        print("| sd [sd]  . . . . . . . . . . . . . show services discovery or toggle")
+        print("| play . . . . . . . . . . . . . . . . . . . . . . . . . . play stream")
+        print("| stop . . . . . . . . . . . . . . . . . . . . . . . . . . stop stream")
+        print("| next . . . . . . . . . . . . . . . . . . . . . .  next playlist item")
+        print("| prev . . . . . . . . . . . . . . . . . . . .  previous playlist item")
+        print("| goto, gotoitem . . . . . . . . . . . . . . . . .  goto item at index")
+        print("| repeat [on|off]  . . . . . . . . . . . . . .  toggle playlist repeat")
+        print("| loop [on|off]  . . . . . . . . . . . . . . . .  toggle playlist loop")
+        print("| random [on|off]  . . . . . . . . . . . . . .  toggle playlist random")
+        print("| clear  . . . . . . . . . . . . . . . . . . . . .  clear the playlist")
+        print("| title [X]  . . . . . . . . . . . . . . set/get title in current item")
+        print("| chapter [X]  . . . . . . . . . . . . set/get chapter in current item")
+        print("| ")
+        print("| seek X . . . . . . . . . . . seek in seconds, for instance `seek 12'")
+        print("| pause  . . . . . . . . . . . . . . . . . . . . . . . .  toggle pause")
+        print("| fastforward  . . . . . . . . . . . . . . . . . . set to maximum rate")
+        print("| rewind . . . . . . . . . . . . . . . . . . . . . set to minimum rate")
+        print("| faster . . . . . . . . . . . . . . . . . .  faster playing of stream")
+        print("| slower . . . . . . . . . . . . . . . . . .  slower playing of stream")
+        print("| normal . . . . . . . . . . . . . . . . . .  normal playing of stream")
+        print("| rate [playback rate] . . . . . . . . . .  set playback rate to value")
+        print("| frame  . . . . . . . . . . . . . . . . . . . . . play frame by frame")
+        print("| fullscreen, f, F [on|off]  . . . . . . . . . . . . toggle fullscreen")
+        print("| ")
+        print("| volume [X] . . . . . . . . . . . . . . . . . .  set/get audio volume")
+        print("| volup [X]  . . . . . . . . . . . . . . .  raise audio volume X steps")
+        print("| voldown [X]  . . . . . . . . . . . . . .  lower audio volume X steps")
+        print("| achan [X]  . . . . . . . . . . . .  set/get stereo audio output mode")
+        print("| atrack [X] . . . . . . . . . . . . . . . . . . . set/get audio track")
+        print("| vtrack [X] . . . . . . . . . . . . . . . . . . . set/get video track")
+        print("| vratio [X] . . . . . . . . . . . . . . .  set/get video aspect ratio")
+        print("| vcrop, crop [X]  . . . . . . . . . . . . . . . .  set/get video crop")
+        print("| vzoom, zoom [X]  . . . . . . . . . . . . . . . .  set/get video zoom")
+        print("| vdeinterlace [X] . . . . . . . . . . . . . set/get video deinterlace")
+        print("| vdeinterlace_mode [X]  . . . . . . .  set/get video deinterlace mode")
+        print("| snapshot . . . . . . . . . . . . . . . . . . . . take video snapshot")
+        print("| strack [X] . . . . . . . . . . . . . . . . .  set/get subtitle track")
+        print("| ")
+        print("| vlm  . . . . . . . . . . . . . . . . . . . . . . . . .  load the VLM")
+        print("| logout . . . . . . . . . . . . . .  exit (if in a socket connection)")
+        print("| quit . . . . . . . .  quit VLC (or logout if in a socket connection)")
+        print("| shutdown . . . . . . . . . . . . . . . . . . . . . . .  shutdown VLC")
+        print()
+    elif(x == chatID + '/: x'):
+        print("Exiting")
+        sys.exit()
+    else:
+        if(serverChoice == 1):
+            try:
+                publish.single(mqttTopic, x, hostname=mqttBrokerIP, port=int(mqttPort))
+            except Exception as e:
+                print("\033[1;38;2;227;32;32mError while sending message\033[0m")   
+                print(str(e)+"\n")
 
-		else:
-			authen = {'username':mqttUsername, 'password':mqttPassword}
-			try:
-				publish.single(mqttTopic, x, hostname=mqttBrokerIP, auth=authen, port=int(mqttPort))
-			except Exception as e:
-				print("\033[1;38;2;227;32;32mError while sending message\033[0m")	
-				print(str(e)+"\n")
+        else:
+            authen = {'username':mqttUsername, 'password':mqttPassword}
+            try:
+                publish.single(mqttTopic, x, hostname=mqttBrokerIP, auth=authen, port=int(mqttPort))
+            except Exception as e:
+                print("\033[1;38;2;227;32;32mError while sending message\033[0m")   
+                print(str(e)+"\n")
--- vlcMQTTSync.py
+++ vlcMQTTSync.py
@@ -23,117 +23,117 @@ def on_message_print(client, userdata, message):
     
     # will be updated in future
     if(message.payload[2:-1] == "sync"):
-    	sendCommand("pause")
-    	time.sleep(0.5)
-    	sendCommand("get_time")
-    	x = 0
-    	while x == 0:
-    		recBuff = tnh.read_very_eager().decode().split("\r\n")
-    		for element in recBuff:
-    			try:
-    				x = int(element)
-    				break
-    			except:
-    				pass
+        sendCommand("pause")
+        time.sleep(0.5)
+        sendCommand("get_time")
+        x = 0
+        while x == 0:
+            recBuff = tnh.read_very_eager().decode().split("\r\n")
+            for element in recBuff:
+                try:
+                    x = int(element)
+                    break
+                except:
+                    pass
     else:
-    	sendCommand(str(message.payload))
+        print(message.payload)
+        sendCommand(str(message.payload[str(message.payload).find('/: ')+3:]))
 
 # for sending commands to VLC telnet host
 def sendCommand(message):
-	global tnh
-	print("Sending: " + message)
-	tnh.write(message.encode('ascii') + b"\n")
-	recBuff = tnh.read_very_eager().decode().split("\r\n")
-	#print(recBuff)
+    global tnh
+    tnh.write(message.encode('ascii') + b"\n")
+    recBuff = tnh.read_very_eager().decode().split("\r\n")
+    #print(recBuff)
 
 def startHostServer():
-	try:
-		cmd = "vlc -I telnet --telnet-password=" + str(hostPassword) + " --telnet-host="+ str(hostIP) + " --telnet-port=" + str(hostPort)
-		process = subprocess.Popen(args = cmd, stdout = subprocess.PIPE, universal_newlines = True, shell = True)
-		#print(process[0])
-	except:
-		print(RED+"Error occured in starting server! Please make sure no instances of VLC are running"+NC)
-		# Exit the program. There's no point continuing. Recursion can reach maximum recursion depths and/or create core dumps.
-		sys.exit()
+    try:
+        cmd = "vlc -I telnet --telnet-password=" + str(hostPassword) + " --telnet-host="+ str(hostIP) + " --telnet-port=" + str(hostPort)
+        process = subprocess.Popen(args = cmd, stdout = subprocess.PIPE, universal_newlines = True, shell = True)
+        #print(process[0])
+    except:
+        print(RED+"Error occured in starting server! Please make sure no instances of VLC are running"+NC)
+        # Exit the program. There's no point continuing. Recursion can reach maximum recursion depths and/or create core dumps.
+        sys.exit()
 
 def connectToHost():
-	global tnh, hostPassword
-	try:
-		print("Attempting Host Telnet Connection...")
-		tnh = telnetlib.Telnet(hostIP, int(hostPort))
-		# give some time before trying to read
-		time.sleep(0.5)
-		try:
-			tnh.read_until(b"Password: ")
-			tnh.write(hostPassword.encode('ascii') + b"\n")
-			tnh.read_until(b"> ")
-			print(GREEN+"Connected To Host"+NC)
-		except:
-			print("Wrong Password!")
-			hostPassword = getpass.getpass()
-			connectToHost()
-	except:
-		t = 3
-		while(t > 0):
-			print("Host server is not yet ready! Retrying in " + str(t) + " seconds...")
-			time.sleep(1)
-			t -= 1
-			print("\033[F\033[K", end = '')
-		print("\033[F\033[K", end = '')
-		connectToHost()
+    global tnh, hostPassword
+    try:
+        print("Attempting Host Telnet Connection...")
+        tnh = telnetlib.Telnet(hostIP, int(hostPort))
+        # give some time before trying to read
+        time.sleep(0.5)
+        try:
+            tnh.read_until(b"Password: ")
+            tnh.write(hostPassword.encode('ascii') + b"\n")
+            tnh.read_until(b"> ")
+            print(GREEN+"Connected To Host"+NC)
+        except:
+            print("Wrong Password!")
+            hostPassword = getpass.getpass()
+            connectToHost()
+    except:
+        t = 3
+        while(t > 0):
+            print("Host server is not yet ready! Retrying in " + str(t) + " seconds...")
+            time.sleep(1)
+            t -= 1
+            print("\033[F\033[K", end = '')
+        print("\033[F\033[K", end = '')
+        connectToHost()
 
 def storeConfigFile(name):
-	shelfFile = shelve.open(os.path.join('config',name+'.config'))
-	if(serverChoice == 1):
-		shelfFile['data'] = [serverChoice, mqttBrokerIP, mqttPort, mqttTopic, hostIP, hostPort, hostPassword]
-	else:
-		shelfFile['data'] = [serverChoice, mqttBrokerIP, mqttPort, mqttTopic, mqttUsername, mqttPassword, hostIP, hostPort, hostPassword]
+    shelfFile = shelve.open(os.path.join('config',name+'.config'))
+    if(serverChoice == 1):
+        shelfFile['data'] = [serverChoice, mqttBrokerIP, mqttPort, mqttTopic, hostIP, hostPort, hostPassword]
+    else:
+        shelfFile['data'] = [serverChoice, mqttBrokerIP, mqttPort, mqttTopic, mqttUsername, mqttPassword, hostIP, hostPort, hostPassword]
 
 def promptSaveConfig():
-	x = input("Do you want to save the current configuration?(y/n) : ")
-	if(x == 'y'):
-		name = input("Give a name to the configuration : ")
-		storeConfigFile(name)
+    x = input("Do you want to save the current configuration?(y/n) : ")
+    if(x == 'y'):
+        name = input("Give a name to the configuration : ")
+        storeConfigFile(name)
 
 def getConfigFiles():
-	global configFiles
-	configFiles = []
-	files = os.listdir(os.path.join(os.getcwd(),'config'))
-	for file in files:
-		if file.endswith('.config'):
-			configFiles.append(file)
-			print(str(len(configFiles)) + ". " + configFiles[len(configFiles) - 1])
+    global configFiles
+    configFiles = []
+    files = os.listdir(os.path.join(os.getcwd(),'config'))
+    for file in files:
+        if file.endswith('.config'):
+            configFiles.append(file)
+            print(str(len(configFiles)) + ". " + configFiles[len(configFiles) - 1])
 
 def loadFromConfig():
-	global configFiles, serverChoice, mqttBrokerIP, mqttPort, mqttTopic, mqttUsername, mqttPassword, hostIP, hostPort, hostPassword
-	if(len(configFiles) > 0):
-		x = input("Do you want to load any previous configurations?<y/n> : ")
-		if(x == 'y'):
-			fileNumber = int(input("Enter corresponding number of file : "))
-			if((fileNumber) <= len(configFiles)):
-				shelfFile = shelve.open(os.path.join('config', configFiles[fileNumber - 1]))
-				if(list(shelfFile['data'])[0] == 1):
-					# public server
-					[serverChoice, mqttBrokerIP, mqttPort, mqttTopic, hostIP, hostPort, hostPassword] = list(shelfFile['data'])
-					return True
-				else:
-					# private server
-					[serverChoice, mqttBrokerIP, mqttPort, mqttTopic, mqttUsername, mqttPassword, hostIP, hostPort, hostPassword] = list(shelfFile['data'])
-					return True
-			else:
-				return False
-		else:
-			return False
+    global configFiles, serverChoice, mqttBrokerIP, mqttPort, mqttTopic, mqttUsername, mqttPassword, hostIP, hostPort, hostPassword
+    if(len(configFiles) > 0):
+        x = input("Do you want to load any previous configurations?<y/n> : ")
+        if(x == 'y'):
+            fileNumber = int(input("Enter corresponding number of file : "))
+            if((fileNumber) <= len(configFiles)):
+                shelfFile = shelve.open(os.path.join('config', configFiles[fileNumber - 1]))
+                if(list(shelfFile['data'])[0] == 1):
+                    # public server
+                    [serverChoice, mqttBrokerIP, mqttPort, mqttTopic, hostIP, hostPort, hostPassword] = list(shelfFile['data'])
+                    return True
+                else:
+                    # private server
+                    [serverChoice, mqttBrokerIP, mqttPort, mqttTopic, mqttUsername, mqttPassword, hostIP, hostPort, hostPassword] = list(shelfFile['data'])
+                    return True
+            else:
+                return False
+        else:
+            return False
 
 def printConfig():
-	print("MQTT Broker IP/Server: "+ mqttBrokerIP)
-	print("MQTT Port: "+ mqttPort)
-	print("MQTT Topic: "+ mqttTopic)
-	print("MQTT Username: "+ mqttUsername)
-	print("MQTT Password: "+ mqttPassword)
-	print("VLC Host IP: "+ hostIP)
-	print("Host Port: "+ hostPort)
-	print("Host Password: "+ hostPassword)
+    print("MQTT Broker IP/Server: "+ mqttBrokerIP)
+    print("MQTT Port: "+ mqttPort)
+    print("MQTT Topic: "+ mqttTopic)
+    print("MQTT Username: "+ mqttUsername)
+    print("MQTT Password: "+ mqttPassword)
+    print("VLC Host IP: "+ hostIP)
+    print("Host Port: "+ hostPort)
+    print("Host Password: "+ hostPassword)
 
 
 # if arguments have been passed, prepare list
@@ -144,58 +144,58 @@ n = len(args)
 
 # command was directly executed
 if(n == 1):
-	getConfigFiles()
-	if(not loadFromConfig()):
-		print("1. Public Server \nor\n2. Private Server\nPublic Servers don't use a username and password.")
-		serverChoice = int(input("Choose (1 or 2): "))
-		if(serverChoice == 1):
-			mqttBrokerIP = input("MQTT Broker IP/Server: ")
-			mqttPort = input("MQTT Port: ")
-			mqttTopic = input("MQTT Topic: ")
-			hostIP = input("VLC Host IP: ")
-			hostPort = input("Host Port: ")
-			print("Host ", end='')
-			sys.stdout.flush()
-			hostPassword = getpass.getpass()
-			promptSaveConfig()
-		elif(serverChoice == 2):
-			mqttBrokerIP = input("MQTT Broker IP/Server: ")
-			mqttPort = input("MQTT Port: ")
-			mqttTopic = input("MQTT Topic: ")
-			mqttUsername = input("MQTT Username: ")
-			print("MQTT ", end='')
-			sys.stdout.flush()
-			mqttPassword = getpass.getpass()
-			hostIP = input("VLC Host IP: ")
-			hostPort = input("Host Port: ")
-			print("Host ", end='')
-			sys.stdout.flush()
-			hostPassword = getpass.getpass()
-			promptSaveConfig()
-		else:
-			sys.exit()
-	else:
-		print("Loaded Configuration:")
-		printConfig()
+    getConfigFiles()
+    if(not loadFromConfig()):
+        print("1. Public Server \nor\n2. Private Server\nPublic Servers don't use a username and password.")
+        serverChoice = int(input("Choose (1 or 2): "))
+        if(serverChoice == 1):
+            mqttBrokerIP = input("MQTT Broker IP/Server: ")
+            mqttPort = input("MQTT Port: ")
+            mqttTopic = input("MQTT Topic: ")
+            hostIP = input("VLC Host IP: ")
+            hostPort = input("Host Port: ")
+            print("Host ", end='')
+            sys.stdout.flush()
+            hostPassword = getpass.getpass()
+            promptSaveConfig()
+        elif(serverChoice == 2):
+            mqttBrokerIP = input("MQTT Broker IP/Server: ")
+            mqttPort = input("MQTT Port: ")
+            mqttTopic = input("MQTT Topic: ")
+            mqttUsername = input("MQTT Username: ")
+            print("MQTT ", end='')
+            sys.stdout.flush()
+            mqttPassword = getpass.getpass()
+            hostIP = input("VLC Host IP: ")
+            hostPort = input("Host Port: ")
+            print("Host ", end='')
+            sys.stdout.flush()
+            hostPassword = getpass.getpass()
+            promptSaveConfig()
+        else:
+            sys.exit()
+    else:
+        print("Loaded Configuration:")
+        printConfig()
 
 # command was executed with arguments
 elif(n == 9):
-	mqttBrokerIP = args[1]
-	mqttPort = args[2]
-	mqttTopic = args[3]
-	mqttUsername = args[4]
-	mqttPassword = args[5]
-	hostIP = args[6]
-	hostPort = args[7]
-	hostPassword = args[8]
-	promptSaveConfig()
+    mqttBrokerIP = args[1]
+    mqttPort = args[2]
+    mqttTopic = args[3]
+    mqttUsername = args[4]
+    mqttPassword = args[5]
+    hostIP = args[6]
+    hostPort = args[7]
+    hostPassword = args[8]
+    promptSaveConfig()
 
 # wrong number of arguments
 else:
-	print("Wrong number of arguments!")
-	print("Usage:\npython3 vlcMQTTSync.py\nor\npython3 vlcMQTTSync.py <mqttBrokerIP> <mqttPort> <mqttTopic> <mqttUsername> <mqttPassword> <hostIP> <hostPassword> <hostPort>")
-	print("Exiting...")
-	sys.exit()
+    print("Wrong number of arguments!")
+    print("Usage:\npython3 vlcMQTTSync.py\nor\npython3 vlcMQTTSync.py <mqttBrokerIP> <mqttPort> <mqttTopic> <mqttUsername> <mqttPassword> <hostIP> <hostPassword> <hostPort>")
+    print("Exiting...")
+    sys.exit()
 
 # create a lock until the user kills all instances of VLC
 x = input("Please make sure no instances of VLC are running. Press Enter key to continue")
@@ -206,15 +206,15 @@ connectToHost()
 print("You may now start a VLC session")
 
 if(serverChoice == 1):
-	try:
-		subscribe.callback(on_message_print, mqttTopic, hostname=mqttBrokerIP, port=int(mqttPort))
-	except:
-		print("Network Error!")
-		sys.exit()
+    try:
+        subscribe.callback(on_message_print, mqttTopic, hostname=mqttBrokerIP, port=int(mqttPort))
+    except:
+        print("Network Error!")
+        sys.exit()
 else:
-	authen = {'username':mqttUsername, 'password':mqttPassword}
-	try:
-		subscribe.callback(on_message_print, mqttTopic, hostname=mqttBrokerIP, auth=authen, port=int(mqttPort))
-	except:
-		print("Network Error!")
-		sys.exit()
+    authen = {'username':mqttUsername, 'password':mqttPassword}
+    try:
+        subscribe.callback(on_message_print, mqttTopic, hostname=mqttBrokerIP, auth=authen, port=int(mqttPort))
+    except:
+        print("Network Error!")
+        sys.exit()
